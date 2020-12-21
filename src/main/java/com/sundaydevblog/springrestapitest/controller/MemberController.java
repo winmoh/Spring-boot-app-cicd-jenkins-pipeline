59f356e2b05a80f99c054b28ed25a9d70a588f87
gouzal <larbi-inco@live.fr>
2020-12-21 18:56:59 -0500

    edit my api

--- src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
+++ src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 
     @GetMapping(value = "/hello")
     public String sayHi(){
-        return "Say hello!";
+        return "Say hello to my little friend !";
     }
 
 }
