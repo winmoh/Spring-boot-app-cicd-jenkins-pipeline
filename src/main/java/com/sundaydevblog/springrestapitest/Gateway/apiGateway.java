46f3dd41a9c44537cd88549aa4a262f44953fbd9
winmoh <mohamedaouinti2023@gmail.com>
2023-12-19 23:18:44 +0100

    updating  api gateway

--- src/main/java/com/sundaydevblog/springrestapitest/Gateway/apiGateway.java
+++ src/main/java/com/sundaydevblog/springrestapitest/Gateway/apiGateway.java
@@ -1,4 +1,9 @@
 package com.sundaydevblog.springrestapitest.Gateway;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/gateway")
+@RestController
 public class apiGateway {
 }
